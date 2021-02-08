12b7ee0f17453b42fe95035725939a14fd1aecc0
Mark <mark.hoogkamer@gmail.com>
2021-02-08 10:45:22 +0100

    Template JS

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,24 @@ document.getElementById("run").addEventListener("click", async () => {
     console.log(main);
 
 })
+
+// NOT READY YET, PLACEHOLDER
+getData().then((pokemons) => {
+
+    pokemons.forEach(pokemon => {
+
+        let template = document.getElementById("pokedex-tpl");
+        let clone = template.content.cloneNode(true);
+
+        let name = clone.querySelector(".name");
+        name.innerText = pokemon.name;
+
+        let id = clone.querySelector(".id");
+        id.innerText = pokemon.id;
+
+        let abilities = clone.querySelector(".abilities");
+        abilities.innerText = pokemon.abilities.join(", ");
+
+        document.getElementById("target").appendChild(clone);
+    })
+})
